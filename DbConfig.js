d9dcdb632723d165f58599e1501800e387db60b1
abdirhman615 <abdirhman615@gmail.com>
2024-01-22 11:51:35 +0300

    test

--- DbConfig.js
+++ DbConfig.js
@@ -4,7 +4,8 @@ mongoose.pluralize(null)
 const DbConnect = async ()=>{
 
     try {
-        await mongoose.connect("mongodb://127.0.0.1:27017/WaterBiling")
+         await mongoose.connect("mongodb://127.0.0.1:27017/WaterBiling")
+        // await mongoose.connect("mongodb+srv://test:123@cluster0.uiltpzx.mongodb.net/WaterBiling")
         console.log("database connected")
 
     } catch (error) {
